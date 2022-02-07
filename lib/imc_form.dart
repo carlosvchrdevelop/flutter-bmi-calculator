ff8560028d8240ab057df23eb0cf70052379ac5a
carlosvchrdevelop <carlosvchrdevelop@gmail.com>
2022-02-07 01:19:00 +0100

    Refactorizado de imports y corregido label de Textfield Peso

--- lib/imc_form.dart
+++ lib/imc_form.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bmi_calculator/imc_textfield.dart';
 
 class IMCForm extends StatelessWidget {
@@ -17,7 +16,7 @@ class IMCForm extends StatelessWidget {
               const SizedBox(height: 50),
               const IMCTextField(label: 'Estatura (cm)'),
               const SizedBox(height: 40),
-              const IMCTextField(label: 'Pesoc(Kg)'),
+              const IMCTextField(label: 'Peso (Kg)'),
               const SizedBox(height: 30),
               ElevatedButton(
                   onPressed: () => {}, child: const Text('Calcular')),
